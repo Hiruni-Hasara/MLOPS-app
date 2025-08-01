6926f1b61b5f5e190c2016904f9516154b2a2088
Hiruni Weliwitiya <hasaraweliwitiya@gmail.com>
2025-08-01 23:05:52 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,10 +1,16 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
 import joblib
 
 # Load dataset
 url = "https://raw.githubusercontent.com/Hiruni-Hasara/hosted-datasets/refs/heads/main/heart.csv"
 df = pd.read_csv(url)
+
+for col in df.columns:
+    if df[col].dtype == 'object':
+        le = LabelEncoder()
+        df[col] = le.fit_transform(df[col])
 
 # Features and target
 X = df.drop("HeartDisease", axis=1)
